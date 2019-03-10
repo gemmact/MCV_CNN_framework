b1fe5d0a4dbef9012057137b68af3eb613a26107
gemma <gemma@vilynx.com>
2019-03-10 02:26:24 +0100

    add yml combinations and jobs

--- job_densenet.sh
+++ job_densenet.sh
@@ -6,6 +6,6 @@
 #SBATCH -o logs/%x_%u_%j.out # File to which STDOUT will be written
 #SBATCH -e logs/%x_%u_%j.err # File to which STDERR will be written
 
-python main.py --exp_name VGG_16 --exp_folder ./experiments/ --config_file ./config/vgg16_tt100k.yml
+python main.py --exp_name densenet161 --exp_folder ./experiments/ --config_file ./config/densenet161_kitti.yml
 
 
